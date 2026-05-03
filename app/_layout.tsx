997f5083df8c1800b52483149cbc0311822b376d
Suleman <msulemanbutt39@gmail.com>
2026-05-03 17:14:52 +0500

    App structure (#1)
    * chore: add initial game folder structure
    
    * chore added some structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
+// this is root wrapper
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </GestureHandlerRootView>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// This is game screen
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>FillFlow 🎮</Text>
+    </View>
+  );
+}
